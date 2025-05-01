4540b206a1917f2f81f2a20bdeb652c2808f1b71
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-05-01 17:58:53 +0600

    Update task_manager.py

--- plugins/task_manager.py
+++ plugins/task_manager.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from bot import Bot
+from bot import Bot, feed_status
 from plugins.db import add_task, remove_task
 
 @Bot.on_message(filters.command("addtask"))
@@ -14,3 +14,13 @@ async def delete(bot, msg: Message):
     title = " ".join(msg.command[1:])
     await remove_task(title)
     await msg.reply(f"❌ Removed: `{title}`")
+
+@Bot.on_message(filters.command("taskon"))
+async def task_on(bot, msg: Message):
+    feed_status["enabled"] = True
+    await msg.reply("RSS AutoFeed: ✅ <b>Enabled</b>")
+
+@Bot.on_message(filters.command("taskoff"))
+async def task_off(bot, msg: Message):
+    feed_status["enabled"] = False
+    await msg.reply("RSS AutoFeed: ❌ <b>Disabled</b>")
