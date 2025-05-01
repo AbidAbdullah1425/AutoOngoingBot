a1185bffe591973157fd33a83eeea55f00225a1f
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-05-01 19:26:57 +0600

    Update task_manager.py

--- plugins/task_manager.py
+++ plugins/task_manager.py
@@ -14,13 +14,3 @@ async def delete(bot, msg: Message):
     title = " ".join(msg.command[1:])
     await remove_task(title)
     await msg.reply(f"❌ Removed: `{title}`")
-
-@Bot.on_message(filters.command("taskon"))
-async def task_on(bot, msg: Message):
-    feed_status["enabled"] = True
-    await msg.reply("RSS AutoFeed: ✅ <b>Enabled</b>")
-
-@Bot.on_message(filters.command("taskoff"))
-async def task_off(bot, msg: Message):
-    feed_status["enabled"] = False
-    await msg.reply("RSS AutoFeed: ❌ <b>Disabled</b>")
