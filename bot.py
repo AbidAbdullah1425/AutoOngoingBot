5652615480cad8fd9689c38a6f0708b2a5b577c9
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-05-02 16:07:39 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
-# bot.py
 from aiohttp import web
 from plugins import web_server
-from config import API_HASH, API_ID, LOGGER, TELEGRAM_TOKEN, TG_BOT_WORKERS, PORT
 from pyrogram import Client
 from pyrogram.enums import ParseMode
+import sys
 from datetime import datetime
-from plugins.rss_checker import check_feed
 import pyrogram.utils
-import asyncio
-
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
+from config import API_HASH, API_ID, LOGGER, TELEGRAM_TOKEN, TG_BOT_WORKERS, PORT
 
 class Bot(Client):
     def __init__(self):
@@ -17,28 +15,38 @@ class Bot(Client):
             name="Bot",
             api_hash=API_HASH,
             api_id=API_ID,
-            plugins={"root": "plugins"},
+            plugins={
+                "root": "plugins"
+            },
             workers=TG_BOT_WORKERS,
             bot_token=TELEGRAM_TOKEN
         )
         self.LOGGER = LOGGER
-        self.feed_enabled = False  # toggle flag for RSS
 
     async def start(self):
         await super().start()
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info("Bot Running...")
-
+        usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
-        self.loop.create_task(check_feed(self))  # pass self to rss_checker
 
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/codeflix_bots")
+        self.LOGGER(__name__).info(f"""       
+
+
+  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
+ / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
+| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
+ \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
+                                                         
+ 
+                                          """)
+        self.username = usr_bot_me.username
+        # web-response
         app = web.AppRunner(await web_server())
         await app.setup()
-        await web.TCPSite(app, "0.0.0.0", PORT).start()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
-
-bot = Bot()
-bot.run()
